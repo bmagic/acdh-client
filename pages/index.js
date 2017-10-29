8b985cc16bda9cf1efe2841b8e29a6a0ca01a7da
bmagic <baptiste.routhieau@gmail.com>
2017-10-29 22:54:24 +0100

    Add warning message on site alpha version

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,9 @@ class HomePage extends React.Component {
         <div id='home-page'>
           <HomeHero />
           <section className='section'>
+            <div className='notification is-warning content has-text-centered'>
+              Ce site en version Alpha est actuellement en développement actif. Merci de ne pas tenir compte des disfonctionnements pour le moment.
+            </div>
             <SearchBar />
             <ProgramsList />
           </section>
