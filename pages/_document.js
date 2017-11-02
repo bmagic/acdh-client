76b175154b058d35a2dc8c1ce176cde16ea7ab7d
bmagic <baptiste.routhieau@gmail.com>
2017-11-02 20:39:37 +0100

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,9 @@ export default class MyDocument extends Document {
           <meta charSet='utf-8' />
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+          <link rel='icon' type='image/png' href='/static/img/favicon/favicon-16x16.png' sizes='16x16' />
+          <link rel='icon' type='image/png' href='/static/img/favicon/favicon-32x32.png' sizes='32x32' />
+          <link rel='icon' type='image/png' href='/static/img/favicon/favicon-96x96.png' sizes='96x96' />
         </Head>
         <body>
           <Main />
